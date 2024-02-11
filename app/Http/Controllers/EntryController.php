cba9449ab536a87f4af222f45121771af03ba1d6
Kacper Duda <kacperduda2002@gmail.com>
2024-02-11 09:20:05 +0100

    Initializing Files

---
+++ app/Http/Controllers/EntryController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Entry;
+use Illuminate\Http\Request;
+
+class EntryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Entry $entry)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Entry $entry)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Entry $entry)
+    {
+        //
+    }
+}
